19a0f1b9ea9945de385d6bba52140f74db3b3750
Matěj Plavina <m.plavina@student.nosch.cz>
2022-01-25 12:48:46 +0100

    Added Osoba.cs

--- GitProjekt2/Program.cs
+++ GitProjekt2/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace GitProjekt2
 {
@@ -6,7 +10,15 @@ namespace GitProjekt2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Osoba osoba = new Osoba();
+
+            Console.WriteLine("Zadej datum narození: ");
+
+            osoba.DatumNarozeni = Convert.ToDateTime(DateTime.Parse(Console.ReadLine()));
+
+            Console.WriteLine(osoba.Vek());
+
+            Console.ReadKey();
         }
     }
 }
